026acf66ec83c08ec37d9b591cec927222f0b378
Tedi Papajorgji <tedi.papajorgji@hotmail.com>
2022-01-14 15:07:19 -0500

    streaming support & tests added

--- src/resemble.js
+++ src/resemble.js
@@ -5,11 +5,13 @@ const APIBuilder = require('./APIBuilder')
  * @param {string} apiToken: The API token to use for authentication.
  * @param {Object} options: An object containing the following options:
  *   - baseUrl (optional): Specifies the base url to use for requests.
+ *   - synServerUrl (optional): Specifies the url to your resemble synthesis server.
  *   - any other version-specific options
  */
 module.exports = function(version, apiToken, options) {
   if (!options) options = {}
   if (options.baseUrl) APIBuilder.setBaseUrl(options.baseUrl)
+  if (options.synServerUrl) APIBuilder.setSynServerUrl(options.synServerUrl)
   
   return APIBuilder.build(version, apiToken, options)
 }
